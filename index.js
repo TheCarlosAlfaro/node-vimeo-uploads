05e54a9b561a68a64b4f28207075c88e7b9881de
devbysalas <rgkalin@gmail.com>
2019-04-27 16:02:40 -0700

    Outputs correct info from Vimeo API to json

--- index.js
+++ index.js
@@ -18,23 +18,24 @@ console.log(demo_date);
 const files_path = '/Users/devbysalas/Dropbox/DEMO day/Videos/';
 
 const demo_day_video_description = `
-Filmed at Demo Day Las Vegas on ${demo_date}. More at https://developers.vegas
+    Filmed at Demo Day Las Vegas on ${demo_date}. More at https://developers.vegas
+    
+    Thanks to our sponsors:
+    
+    Innevation
+    innevation.com/
+    
+    Vehicle History
+    vehiclehistory.com/
+    
+    CHSI Technologies
+    chsiconnections.com/
+    
+    Dot Vegas
+    the.vegas/
+    `;
 
-Thanks to our sponsors:
-
-Innevation
-innevation.com/
-
-Vehicle History
-vehiclehistory.com/
-
-CHSI Technologies
-chsiconnections.com/
-
-Dot Vegas
-the.vegas/
-`;
-
+fs.appendFileSync(`${demo_date} presentations.json`, `[`);
 const all_videos = fs.readdirSync(files_path);
 
 all_videos.sort(function(a, b) {
@@ -56,21 +57,62 @@ function upload_videos(videos_dir, all_videos, video_info) {
         description: `${demo_day_video_description}`
       },
       function(uri) {
+        // callback when completed
         console.log('Your video URI is: ' + uri);
 
         vimeo_account.request(
           {
+            // Adds video to channel
             method: 'PUT',
             path: `/channels/1449073${uri}`
           },
           function(error, body, status_code, headers) {
             if (error) {
-              console.log(error, body, status_code, headers);
+              console.log(error);
             }
 
             console.log(body);
           }
         );
+        vimeo_account.request(
+          {
+            // Gets all video info
+            method: 'GET',
+            path: uri
+          },
+          function(error, body, status_code, headers) {
+            if (error) {
+              console.log(error);
+            }
+
+            const video_obj = video_info[index];
+
+            video_obj.presentation.video_screenshot_url =
+              body.pictures.sizes[2].link;
+            video_obj.presentation.video_screenshot_with_play_url =
+              body.pictures.sizes[2].link_with_play_button;
+            video_obj.presentation.video_url = body.link;
+            video_obj.presentation.video_iframe = body.embed.html;
+
+            function end_bracket() {
+              const new_index = index + 1;
+              console.log(new_index, video_info.length);
+
+              if (new_index === video_info.length) {
+                return `]`;
+              } else {
+                return '';
+              }
+            }
+
+            fs.appendFileSync(
+              `${demo_date} presentations.json`,
+              `${JSON.stringify(video_obj)}, ${end_bracket()}`
+            );
+            // console.log('This is the video GET request', video_obj);
+          }
+        );
+        // console.log('This is all presentations', all_presentations);
       },
       function(bytes_uploaded, bytes_total) {
         var percentage = ((bytes_uploaded / bytes_total) * 100).toFixed(2);
