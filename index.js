9da5c096945901a0748d29bbf6506ac8eb9220b4
devbysalas <rgkalin@gmail.com>
2019-06-18 16:38:21 -0700

    Fixes issue with outputting .json file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+// Imports API keys from .env file
 require('dotenv').config();
+// Imports presenters list (should get it from an API point in the future)
 const presenter_list = require('./presenter_list.js');
 const fs = require('fs');
 const Vimeo = require('vimeo').Vimeo;
@@ -13,9 +15,10 @@ const demo_date = date_fns.format(
   presenter_list[0].event.date,
   'MMMM Do, YYYY'
 );
-console.log(demo_date);
 
-const files_path = '/Users/devbysalas/Dropbox/DEMO day/real-videos/';
+// Path to folder where videos are located.
+// const files_path = '/Users/devbysalas/Dropbox/DEMO day/real-videos/';
+const files_path = '/Users/devbysalas/Dropbox/DEMO day/Videos/';
 
 const demo_day_video_description = `
     Filmed at Demo Day Las Vegas on ${demo_date}. More at https://developers.vegas
@@ -35,9 +38,9 @@ const demo_day_video_description = `
     https://the.vegas/
     `;
 
-fs.appendFileSync(`${demo_date} presentations.json`, `[`);
+// Stores all videos in files_path directory to a variable.
 const all_videos = fs.readdirSync(files_path);
-
+// Sorts videos by the time/date - puts them in order so it matches presenter_list.js
 all_videos.sort(function(a, b) {
   return (
     fs.statSync(files_path + a).mtime.getTime() -
@@ -45,6 +48,10 @@ all_videos.sort(function(a, b) {
   );
 });
 
+// We will push each video_obj after upload to this array
+const vimeo_response = [];
+
+// Main upload function
 function upload_videos(videos_dir, all_videos, video_info) {
   all_videos.forEach((video, index) => {
     let file_name = `${videos_dir}${video}`;
@@ -59,21 +66,22 @@ function upload_videos(videos_dir, all_videos, video_info) {
       function(uri) {
         // callback when completed
         console.log('Your video URI is: ' + uri);
+        // I uncomment this request when using developers.vegas vimeo account.
+        // Adds videos to a channel.
+        // vimeo_account.request(
+        //   {
+        //     // Adds video to channel
+        //     method: 'PUT',
+        //     path: `/channels/1449073${uri}`
+        //   },
+        //   function(error, body, status_code, headers) {
+        //     if (error) {
+        //       console.log(error);
+        //     }
 
-        vimeo_account.request(
-          {
-            // Adds video to channel
-            method: 'PUT',
-            path: `/channels/1449073${uri}`
-          },
-          function(error, body, status_code, headers) {
-            if (error) {
-              console.log(error);
-            }
-
-            console.log(body);
-          }
-        );
+        //     console.log(body);
+        //   }
+        // );
         vimeo_account.request(
           {
             // Gets all video info
@@ -85,6 +93,7 @@ function upload_videos(videos_dir, all_videos, video_info) {
               console.log(error);
             }
 
+            // we create an object per video and add properties with the response from vimeo.
             const video_obj = video_info[index];
 
             video_obj.presentation.video_screenshot_url =
@@ -94,25 +103,23 @@ function upload_videos(videos_dir, all_videos, video_info) {
             video_obj.presentation.video_url = body.link;
             video_obj.presentation.video_iframe = body.embed.html;
 
-            function end_bracket() {
-              const new_index = index + 1;
-              console.log(new_index, video_info.length);
+            vimeo_response.push(video_obj);
 
-              if (new_index === video_info.length) {
-                return `]`;
-              } else {
-                return '';
-              }
+            // Creates a .json file when uploads are done.
+            if (vimeo_response.length == presenter_list.length) {
+              console.log(
+                `${demo_date} presentations.json should be ready soon`
+              );
+
+              const vimeo_res_JSON = JSON.stringify(vimeo_response);
+              // Creates a .json file where respond from vimeo after upload will be outputted to.
+              fs.appendFileSync(
+                `${demo_date} presentations.json`,
+                vimeo_res_JSON
+              );
             }
-
-            fs.appendFileSync(
-              `${demo_date} presentations.json`,
-              `${JSON.stringify(video_obj)}, ${end_bracket()}`
-            );
-            // console.log('This is the video GET request', video_obj);
           }
         );
-        // console.log('This is all presentations', all_presentations);
       },
       function(bytes_uploaded, bytes_total) {
         var percentage = ((bytes_uploaded / bytes_total) * 100).toFixed(2);
--- presenter_list.js
+++ presenter_list.js
@@ -1,11 +1,11 @@
 const presenter_list = [
   {
-    event: { date: '2019-04-06' },
+    event: { date: '2019-06-18' },
     member: { row_id: 366, first_name: 'Eugene', last_name: 'Bondari' },
     presentation: { title: 'Tree Iteration', is_active: true, order: 0 }
   },
   {
-    event: { date: '2019-04-06' },
+    event: { date: '2019-06-18' },
     member: { row_id: 304, first_name: 'Andrew', last_name: 'Yasso' },
     presentation: { title: 'Before & After Photos', is_active: true, order: 1 },
     technologies: [
@@ -13,114 +13,114 @@ const presenter_list = [
       { id: 66, name: 'Node.js', popularity: 50 },
       { id: 118, name: 'Chrome extension', popularity: 40 }
     ]
-  },
-  {
-    event: { date: '2019-04-06' },
-    member: { row_id: 433, first_name: 'Edinardo', last_name: 'Potrich' },
-    presentation: {
-      title: 'PowerShift thermostat integration with Google Actions',
-      is_active: true,
-      order: 2
-    },
-    technologies: [
-      { id: 66, name: 'Node.js', popularity: 50 },
-      { id: 34, name: 'Firebase', popularity: 50 }
-    ]
-  },
-  {
-    event: { date: '2019-04-06' },
-    member: { row_id: 321, first_name: 'Nathan', last_name: 'Gelman' },
-    presentation: { title: 'Emoji Grid', is_active: true, order: 3 },
-    technologies: [{ id: 91, name: 'Swift', popularity: 50 }]
-  },
-  {
-    event: { date: '2019-04-06' },
-    member: { row_id: 383, first_name: 'Kyle', last_name: 'Elmore' },
-    presentation: {
-      title: 'A JavaScript Logging Module',
-      is_active: true,
-      order: 4
-    },
-    technologies: [
-      { id: 1, name: 'JavaScript', popularity: 60 },
-      { id: 66, name: 'Node.js', popularity: 50 },
-      { id: 94, name: 'TypeScript', popularity: 50 },
-      { id: 64, name: 'MySQL', popularity: 50 }
-    ]
-  },
-  {
-    event: { date: '2019-04-06' },
-    member: { row_id: 426, first_name: 'Tim', last_name: 'Osterbuhr' },
-    presentation: {
-      title: 'Reviving Crayon Crunch a Personalized Kids Book',
-      is_active: true,
-      order: 5
-    },
-    technologies: [
-      { id: 71, name: 'PHP', popularity: 50 },
-      { id: 75, name: 'Python', popularity: 50 }
-    ]
-  },
-  {
-    event: { date: '2019-04-06' },
-    member: { row_id: 372, first_name: 'Lorenz', last_name: 'Madarang' },
-    presentation: {
-      title: 'Teaching a Machine to Appreciate Adele and Bob Marley',
-      is_active: true,
-      order: 6
-    },
-    technologies: [{ id: 75, name: 'Python', popularity: 50 }]
-  },
-  {
-    event: { date: '2019-04-06' },
-    member: { row_id: 195, first_name: 'Sunny', last_name: 'Clark' },
-    presentation: { title: 'The Platform', is_active: true, order: 7 },
-    technologies: [
-      { id: 66, name: 'Node.js', popularity: 50 },
-      { id: 63, name: 'MongoDB', popularity: 50 }
-    ]
-  },
-  {
-    event: { date: '2019-04-06' },
-    member: { row_id: 286, first_name: 'Carlos', last_name: 'Alfaro' },
-    presentation: { title: 'Learning by doing', is_active: true, order: 8 },
-    technologies: [{ id: 66, name: 'Node.js', popularity: 50 }]
-  },
-  {
-    event: { date: '2019-04-06' },
-    member: { row_id: 292, first_name: 'Tyler', last_name: 'Hanevold' },
-    presentation: {
-      title: 'Untitled project, April 6th, 2019',
-      is_active: true,
-      order: 9
-    },
-    technologies: [{ id: 71, name: 'PHP', popularity: 50 }]
-  },
-  {
-    event: { date: '2019-04-06' },
-    member: { row_id: 142, first_name: 'Mike', last_name: 'Ziethlow' },
-    presentation: {
-      title: 'A open source list of all programming technologies',
-      is_active: true,
-      order: 10
-    },
-    technologies: [
-      { id: 66, name: 'Node.js', popularity: 50 },
-      { id: 1, name: 'JavaScript', popularity: 60 },
-      { id: 79, name: 'React', popularity: 50 },
-      { id: 47, name: 'HTML', popularity: 50 },
-      { id: 26, name: 'CSS', popularity: 50 }
-    ]
-  },
-  {
-    event: { date: '2019-04-06' },
-    member: { row_id: 343, first_name: 'Andy', last_name: 'Goldberg' },
-    presentation: {
-      title: 'Reveal.js - a Powerpoint alternative',
-      is_active: true,
-      order: 11
-    }
   }
+  // {
+  //   event: { date: '2019-04-06' },
+  //   member: { row_id: 433, first_name: 'Edinardo', last_name: 'Potrich' },
+  //   presentation: {
+  //     title: 'PowerShift thermostat integration with Google Actions',
+  //     is_active: true,
+  //     order: 2
+  //   },
+  //   technologies: [
+  //     { id: 66, name: 'Node.js', popularity: 50 },
+  //     { id: 34, name: 'Firebase', popularity: 50 }
+  //   ]
+  // },
+  // {
+  //   event: { date: '2019-04-06' },
+  //   member: { row_id: 321, first_name: 'Nathan', last_name: 'Gelman' },
+  //   presentation: { title: 'Emoji Grid', is_active: true, order: 3 },
+  //   technologies: [{ id: 91, name: 'Swift', popularity: 50 }]
+  // },
+  // {
+  //   event: { date: '2019-04-06' },
+  //   member: { row_id: 383, first_name: 'Kyle', last_name: 'Elmore' },
+  //   presentation: {
+  //     title: 'A JavaScript Logging Module',
+  //     is_active: true,
+  //     order: 4
+  //   },
+  //   technologies: [
+  //     { id: 1, name: 'JavaScript', popularity: 60 },
+  //     { id: 66, name: 'Node.js', popularity: 50 },
+  //     { id: 94, name: 'TypeScript', popularity: 50 },
+  //     { id: 64, name: 'MySQL', popularity: 50 }
+  //   ]
+  // },
+  // {
+  //   event: { date: '2019-04-06' },
+  //   member: { row_id: 426, first_name: 'Tim', last_name: 'Osterbuhr' },
+  //   presentation: {
+  //     title: 'Reviving Crayon Crunch a Personalized Kids Book',
+  //     is_active: true,
+  //     order: 5
+  //   },
+  //   technologies: [
+  //     { id: 71, name: 'PHP', popularity: 50 },
+  //     { id: 75, name: 'Python', popularity: 50 }
+  //   ]
+  // },
+  // {
+  //   event: { date: '2019-04-06' },
+  //   member: { row_id: 372, first_name: 'Lorenz', last_name: 'Madarang' },
+  //   presentation: {
+  //     title: 'Teaching a Machine to Appreciate Adele and Bob Marley',
+  //     is_active: true,
+  //     order: 6
+  //   },
+  //   technologies: [{ id: 75, name: 'Python', popularity: 50 }]
+  // },
+  // {
+  //   event: { date: '2019-04-06' },
+  //   member: { row_id: 195, first_name: 'Sunny', last_name: 'Clark' },
+  //   presentation: { title: 'The Platform', is_active: true, order: 7 },
+  //   technologies: [
+  //     { id: 66, name: 'Node.js', popularity: 50 },
+  //     { id: 63, name: 'MongoDB', popularity: 50 }
+  //   ]
+  // },
+  // {
+  //   event: { date: '2019-04-06' },
+  //   member: { row_id: 286, first_name: 'Carlos', last_name: 'Alfaro' },
+  //   presentation: { title: 'Learning by doing', is_active: true, order: 8 },
+  //   technologies: [{ id: 66, name: 'Node.js', popularity: 50 }]
+  // },
+  // {
+  //   event: { date: '2019-04-06' },
+  //   member: { row_id: 292, first_name: 'Tyler', last_name: 'Hanevold' },
+  //   presentation: {
+  //     title: 'Untitled project, April 6th, 2019',
+  //     is_active: true,
+  //     order: 9
+  //   },
+  //   technologies: [{ id: 71, name: 'PHP', popularity: 50 }]
+  // },
+  // {
+  //   event: { date: '2019-04-06' },
+  //   member: { row_id: 142, first_name: 'Mike', last_name: 'Ziethlow' },
+  //   presentation: {
+  //     title: 'A open source list of all programming technologies',
+  //     is_active: true,
+  //     order: 10
+  //   },
+  //   technologies: [
+  //     { id: 66, name: 'Node.js', popularity: 50 },
+  //     { id: 1, name: 'JavaScript', popularity: 60 },
+  //     { id: 79, name: 'React', popularity: 50 },
+  //     { id: 47, name: 'HTML', popularity: 50 },
+  //     { id: 26, name: 'CSS', popularity: 50 }
+  //   ]
+  // },
+  // {
+  //   event: { date: '2019-04-06' },
+  //   member: { row_id: 343, first_name: 'Andy', last_name: 'Goldberg' },
+  //   presentation: {
+  //     title: 'Reveal.js - a Powerpoint alternative',
+  //     is_active: true,
+  //     order: 11
+  //   }
+  // }
 ];
 
 module.exports = presenter_list;
